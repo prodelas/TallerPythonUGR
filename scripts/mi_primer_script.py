b86b29e3c69c3e17a0b43149ab8c0c6448ce9a70
prodelas <prodelas@gmail.com>
2018-08-23 19:21:05 +0200

    Add files via upload

--- scripts/mi_primer_script.py
+++ scripts/mi_primer_script.py
@@ -1,11 +1,42 @@
-import math
+import matplotlib.pyplot as plt
      
-print("Hola gente del curso de AeroPython")
-print("¿Cuántos sois hoy en clase?")
+print("¡Hola gente del taller de Python!")
+print("¿Cuántos de vosotros sois completamente novatos con Python?")
+nnovatos = input()
+nnovatos = int(nnovatos)
 
-number = input()
-number = int(number)
-root = math.sqrt(number)
+if nnovatos > 20:
+    print("¡¡Ufff, eso es un montón!! Espero que aprendáis mucho hoy... \n")
 
-print("Ufff! eso es un montón! espero que aprendáis mucho")
-print("Por cierto, la raiz de %i es %f" %(number, root))
+
+print("¿Cuántos de vosotros ya os habéis iniciado un poquito en Python?")
+niniciados = input()
+niniciados = int(niniciados)
+print("Bueno, ¡siempre hay algo nuevo que aprender!\n")
+
+
+print("¿Cuántos de vosotros ya sabéis bastante de Python?")
+navanzados = input()
+navanzados  = int(navanzados)
+print("Bueno, en todo caso ¡espero que también aprendáis algo nuevo hoy!\n")
+
+print("Y por último, ¿cuántos de vosotros ya sois casi unos expertos en Python?")
+nexpertos = input()
+nexpertos  = int(nexpertos)
+if nexpertos > 2:
+    print("Bueno,¡en realidad no se qué hacéis vosotros aquí! \n")
+
+
+# Datos a mostrar
+labels = 'Neófitos', 'Iniciados', 'Avanzados', 'Expertos'
+sizes = [nnovatos, niniciados, navanzados, nexpertos]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0, 0, 0)  # explode 1st slice
+
+
+# Gráfico por sectores
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
+plt.show();
