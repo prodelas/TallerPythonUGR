ab39857406f50361bc812237b85a1e8d19c165f5
prodelas <prodelas@gmail.com>
2018-09-26 20:38:30 +0000

    Delete suma.f90

--- SEM_Pythorn_amcastro/suma.f90
+++
@@ -1,9 +0,0 @@
-
-subroutine suma2(n, v)
-    ! Compute the dot product between u and v (length n) and put the result in product
-     
-    integer, intent(in) :: n
-    integer, intent(out) :: v
-    v = n + 2
-    
-end subroutine
